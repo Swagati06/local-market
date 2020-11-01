851d9b469daeb75c12dec25f839bf59d97f1f722
VedantRD <vedant.debadwar@gmail.com>
2020-11-01 12:53:31 +0530

    akbf

--- src/App.js
+++ src/App.js
@@ -1,87 +1,90 @@
 import React from 'react';
 import './App.css';
-import {Header, Navigation,Layout, Drawer, Content } from 'react-mdl'
+import { Header, Navigation, Layout, Drawer, Content } from 'react-mdl'
 import Main from './Components/main'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 function App() {
-  return (
-    <div className="demo-big-content">
-    
+    return (
+        <div className="demo-big-content">
 
-    <Layout>
-        <Header className='header-color' title={<Link style={{textDecoration:'none', color:'white'}} to="/">VENDO</Link>} scroll>
-            
-        <Navigation>
-                <Link to="/resume">SHOP</Link>
-                <Link to="/Sell">SELL</Link>
-                
-                <Link to="/signup">SIGN UP</Link>
-                <Link to="/projects">CART</Link>
-                
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration:'none', color:'black'}} to="/">VENDO</Link>}>
-            <Navigation>
-                <Link to="/resume">My Account</Link>
-                <Link to="/about">Your Order</Link>
-                <Link to="/about">Track Your Order</Link>
-                <Link to="/about">Manage</Link>
-                <Link to="/resume">Help</Link>
-                <Link to="/contact">Contact us</Link>
-                <Link to="/resume">Log Out</Link>
-                
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main />
-        </Content>
-    </Layout>
-    </div>
-  );
+            <Layout>
+                <Header className='header-color' title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">VENDO</Link>} scroll>
 
-function App() {
+                    <Navigation>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                        <Link to="/resume">SHOP</Link>
+                        <Link to="/Sell">SELL</Link>
 
-  return (
-    <div className="demo-big-content">
-    <Layout>
-        <Header className='header-color' title={<Link style={{textDecoration:'none', color:'white'}} to="/"><b>VENDO</b></Link>} scroll>
-            
-        <Navigation>
-                <Link to="/resume"><b>SHOP</b></Link>
-                <Link to="/Sell"><b>SELL</b></Link>
-                <Link to="/projects"><b>CATEGORIES</b></Link>
-                <Link to="/signup"><b>SIGN UP</b></Link>
-                <Link to="/cart"><b>CART</b></Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration:'none', color:'black'}} to="/">VENDO</Link>}>
-            <Navigation>
-                <Link to="/contact">My account</Link>
-                <Link to="/contact">Your Order</Link>
-                <Link to="/resume">Track Your Order</Link>
-                <Link to="/about">Manage</Link>
-                <Link to="/about">About us</Link>
-                <Link to="/contact">Contact us</Link>
-                <Link to="/about">Help</Link>
-                <Link to="/resume">Log Out</Link>
-               
-                {/* <Link to="/contact"></Link> */}
-                
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main />
-        </Content>
-    </Layout>
-    </div>
-  );
+                        <Link to="/signup">SIGN UP</Link>
+                        <Link to="/projects">CART</Link>
 
-   
-  }
+                    </Navigation>
+                </Header>
+                <Drawer title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">VENDO</Link>}>
+                    <Navigation>
+                        <Link to="/resume">My Account</Link>
+                        <Link to="/about">Your Order</Link>
+                        <Link to="/about">Track Your Order</Link>
+                        <Link to="/about">Manage</Link>
+                        <Link to="/resume">Help</Link>
+                        <Link to="/contact">Contact us</Link>
+                        <Link to="/resume">Log Out</Link>
+
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main />
+                </Content>
+            </Layout>
+        </div>
+    );
+
+    function App() {
+
+        return (
+            <div className="demo-big-content">
+                <Layout>
+                    <Header className='header-color' title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/"><b>VENDO</b></Link>} scroll>
+
+                        <Navigation>
+                            <Link to="/resume"><b>SHOP</b></Link>
+                            <Link to="/Sell"><b>SELL</b></Link>
+                            <Link to="/projects"><b>CATEGORIES</b></Link>
+                            <Link to="/signup"><b>SIGN UP</b></Link>
+                            <Link to="/cart"><b>CART</b></Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">VENDO</Link>}>
+                        <Navigation>
+                            <Link to="/contact">My account</Link>
+                            <Link to="/contact">Your Order</Link>
+                            <Link to="/resume">Track Your Order</Link>
+                            <Link to="/about">Manage</Link>
+                            <Link to="/about">About us</Link>
+                            <Link to="/contact">Contact us</Link>
+                            <Link to="/about">Help</Link>
+                            <Link to="/resume">Log Out</Link>
+
+                            {/* <Link to="/contact"></Link> */}
+
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content" />
+                        <Main />
+                    </Content>
+                </Layout>
+            </div>
+        );
+
+
+    }
 
 }
 
--- src/Components/landingPage.js
+++ src/Components/landingPage.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 import Card from './Card.js'
-import styles from '../styles/landinpage.css'
 import Footer from './Footer.js';
 
 const card_contents = [
   { title: <b>CLOTHING</b>, url: "https://images.pexels.com/photos/1078958/pexels-photo-1078958.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
-  { title: <b>GROCERY</b>, url:  "https://media.istockphoto.com/photos/supermarket-aisle-with-empty-shopping-cart-business-concept-picture-id962388052" },
+  { title: <b>GROCERY</b>, url: "https://media.istockphoto.com/photos/supermarket-aisle-with-empty-shopping-cart-business-concept-picture-id962388052" },
   { title: <b>COSMETICS</b>, url: "https://s3-us-west-2.amazonaws.com/commercehq-userfiles-master/commercehq-store-eeaca839583a42e15da19b7fc901a309_da39a3ee5e6b4b0d3255bfef95601890afd80709/fb9eb0a57c0841eeaf66371aab2caf09_make_up_HD1.jpg" },
   { title: <b>HARDWARE</b>, url: "https://media.istockphoto.com/photos/home-appliance-in-the-store-picture-id515443264?k=6&m=515443264&s=612x612&w=0&h=QgzJ5Wq44l5_A-4J1pRm6HJ-3R6Y4d0SxQuNfBdiw4Y=" },
 ]
@@ -17,7 +16,7 @@ class landingPage extends Component {
   render() {
     return (
       <div>
-        <div style={{ backgroundImage: 'url(https://images.pexels.com/photos/1087727/pexels-photo-1087727.jpeg?cs=srgb&dl=pexels-min-an-1087727.jpg&fm=jpg)' , height: 550, backgroundSize: 'cover', }}>
+        <div style={{ backgroundImage: 'url(https://images.pexels.com/photos/1087727/pexels-photo-1087727.jpeg?cs=srgb&dl=pexels-min-an-1087727.jpg&fm=jpg)', height: 550, backgroundSize: 'cover', }}>
 
           <div className="font-txt">
             <h1 style={{ fontSize: 70, fontFamily: 'Anton', textAlign: 'center', color: 'white', paddingTop: 140 }}><i>IT'S AN "ADD TO CART" KINDA DAY !!!</i></h1>
@@ -29,7 +28,7 @@ class landingPage extends Component {
         store as a web and mobile application. And what better resource than content to decorate your online store!
         </b></div>
 
-    
+
         </div>
 
         <div>
@@ -41,7 +40,7 @@ class landingPage extends Component {
             ))}
           </Grid>
         </div>
-          <Footer />
+        <Footer />
       </div>
     );
 
