8c6f9a3d480bdbd78e0aee4328b71165e5b3c0e6
vd25 <vaibhavidalal25@gmail.com>
2020-11-04 17:58:08 +0530

    changes

--- src/Components/sell.js
+++ src/Components/sell.js
@@ -8,11 +8,7 @@ import { Link } from 'react-router-dom'
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
-<<<<<<< HEAD
   { width: 50, itemsToShow: 2 },
-=======
-  { width: 20, itemsToShow: 2 },
->>>>>>> 0195448799b496a14097ea3901f340afb3ac0bdc
   { width: 768, itemsToShow: 3 },
   { width: 1200, itemsToShow: 4 },
 ];
@@ -21,11 +17,8 @@ const cities=["NANDED","PUNE","MUMBAI","AURANGABAD","NASHIK","HYDERABAD","DELHI"
 function sell() {
   return (
     <>
-<<<<<<< HEAD
+
       <h1 style={{  textAlign: "center"}}>FIND YOUR OWN CITY SHOPS!!</h1>
-=======
-      <h1 style={{ textAlign: "center" }}>FIND YOUR OWN CITY SHOPS!!</h1>
->>>>>>> 0195448799b496a14097ea3901f340afb3ac0bdc
       <div className="App">
       
         <Carousel breakPoints={breakPoints}>
@@ -36,11 +29,11 @@ function sell() {
           <Item>NASHIK</Item>
           <Item>HYDRABAD</Item>
           <Item>DELHI</Item> */}
-<<<<<<< HEAD
+
           {cities.map((item,index)=>(<Link to="/page1"><Item style={{width: '140%'}}>{item}</Item></Link>))}
-=======
-          {cities.map((item,index)=>(<Link to="/page1"><Item style={{width: '100%'}}>{item}</Item></Link>))}
->>>>>>> 0195448799b496a14097ea3901f340afb3ac0bdc
+
+          
+
         
         </Carousel>
       </div>
